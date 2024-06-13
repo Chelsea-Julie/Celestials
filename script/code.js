942f1795d2f57fe666b5dd676fe494baab662060
Chelsea-Julie <chelseaqjulie.gmail.com>
2024-06-13 09:15:56 +0200

    stylingNav

--- script/code.js
+++ script/code.js
@@ -1,1 +1,11 @@
 // 
+let header = document.querySelector(".navbar-nav");
+let a = header.getElementsByClassName("nav-link");
+for ( let i = 0; i < a.length; i++) {
+  a [i].addEventListener("click", function() {
+  let current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace("active", "");
+  this.className += "active";
+  });
+}
+
